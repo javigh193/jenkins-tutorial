3a015329c5bf0aec5c14393f87b280fae871b0e0
javigh193 <115082160+javigh193@users.noreply.github.com>
2024-12-30 03:52:43 +0100

    Added imports to Test file

--- src/test/java/com/jenkinstutorial/jenkinstutorial/JenkinstutorialApplicationTests.java
+++ src/test/java/com/jenkinstutorial/jenkinstutorial/JenkinstutorialApplicationTests.java
@@ -1,5 +1,10 @@
 package com.jenkinstutorial.jenkinstutorial;
 
+import static org.junit.Assert.assertEquals;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
