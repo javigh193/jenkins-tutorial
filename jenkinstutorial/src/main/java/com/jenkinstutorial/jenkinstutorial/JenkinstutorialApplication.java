7789e1b7728cb0c19b1b15eae0561e5229297c4b
javigh193 <115082160+javigh193@users.noreply.github.com>
2024-12-30 03:00:46 +0100

    Set some loggers for launching and testing

--- jenkinstutorial/src/main/java/com/jenkinstutorial/jenkinstutorial/JenkinstutorialApplication.java
+++ jenkinstutorial/src/main/java/com/jenkinstutorial/jenkinstutorial/JenkinstutorialApplication.java
@@ -1,12 +1,23 @@
 package com.jenkinstutorial.jenkinstutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinstutorialApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(JenkinstutorialApplication.class);
+
+	@PostConstruct
+	public void intt() {
+		logger.info("Application started...");
+	}
+
 	public static void main(String[] args) {
+		logger.info("Application executed...");
 		SpringApplication.run(JenkinstutorialApplication.class, args);
 	}
 
--- jenkinstutorial/src/test/java/com/jenkinstutorial/jenkinstutorial/JenkinstutorialApplicationTests.java
+++ jenkinstutorial/src/test/java/com/jenkinstutorial/jenkinstutorial/JenkinstutorialApplicationTests.java
@@ -6,8 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JenkinstutorialApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(JenkinstutorialApplicationTest.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
 	}
 
 }
